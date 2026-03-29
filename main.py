2bec3e5f98094fe13564e508883b6cd1c688f9a8
alexbit333-arch <alexbit333@gmail.com>
2026-03-29 02:20:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-TOKEN = "8717329314:AAGo2P9dXE6WT4xygLrrzK-pAytT0Ix-_eM"
-GOOGLE_URL = "https://script.google.com/macros/s/AKfycbzecpCxyABxFy6H4A76BjK-fnQe2Tj6HbJRh55juHRqfz2OcdJ4ZdkMms_oP2xfhkvOfw/exec"
+# 👉 ВСТАВ СЮДИ СВОЇ ДАНІ
+TELEGRAM_TOKEN = "8717329314:AAGo2P9dXE6WT4xygLrrzK-pAytT0Ix-_eM"
+GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzecpCxyABxFy6H4A76BjK-fnQe2Tj6HbJRh55juHRqfz2OcdJ4ZdkMms_oP2xfhkvOfw/exec"
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
@@ -11,18 +12,24 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     data = {
         "user_id": user.id,
-        "username": user.username or "no_username",
+        "username": user.username,
         "text": text
     }
 
     try:
-        requests.post(GOOGLE_URL, json=data)
-        await update.message.reply_text("☀️ Записано в таблицю")
-    except:
-        await update.message.reply_text("❌ Помилка")
+        requests.post(GOOGLE_SCRIPT_URL, json=data)
+        await update.message.reply_text("✅ Записано в таблицю")
+    except Exception as e:
+        await update.message.reply_text("❌ Помилка запису")
+        print(e)
 
-app = ApplicationBuilder().token(TOKEN).build()
+def main():
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-app.run_polling()
+    print("Бот запущений...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
