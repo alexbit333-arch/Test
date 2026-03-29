437c50057ca23cca00d31df17d7109552f2b4539
alexbit333-arch <alexbit333@gmail.com>
2026-03-29 02:25:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import requests
-from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+from telegram.ext import Updater, MessageHandler, Filters
 
-# 👉 ВСТАВ СЮДИ СВОЇ ДАНІ
 TELEGRAM_TOKEN = "8717329314:AAGo2P9dXE6WT4xygLrrzK-pAytT0Ix-_eM"
 GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzecpCxyABxFy6H4A76BjK-fnQe2Tj6HbJRh55juHRqfz2OcdJ4ZdkMms_oP2xfhkvOfw/exec"
 
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.effective_user
+def handle_message(update, context):
+    user = update.message.from_user
     text = update.message.text
 
     data = {
@@ -18,18 +16,20 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     try:
         requests.post(GOOGLE_SCRIPT_URL, json=data)
-        await update.message.reply_text("✅ Записано в таблицю")
+        update.message.reply_text("✅ Записано в таблицю")
     except Exception as e:
-        await update.message.reply_text("❌ Помилка запису")
+        update.message.reply_text("❌ Помилка")
         print(e)
 
 def main():
-    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+    updater = Updater(TELEGRAM_TOKEN, use_context=True)
+    dp = updater.dispatcher
 
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
 
     print("Бот запущений...")
-    app.run_polling()
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == "__main__":
     main()
